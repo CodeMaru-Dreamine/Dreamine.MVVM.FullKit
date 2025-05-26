6ea3d765f1c35eb12d7dab48de63d2d59cfa9b45
msjang <msjang@j-innotech.com>
2025-05-26 23:17:37 +0900

    delete

--- Sample WPF/DreamineApp/Events/PageSub/PageSubEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace DreamineApp.Events.PageSub;
-
-public class PageSubEvent
-{
-	public void Ok() => MessageBox.Show("확인 클릭됨!");
-	public void Cancel() => MessageBox.Show("취소 클릭됨!");
-}
--- Sample WPF/DreamineApp/Models/PageSub/PageSubModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dreamine.MVVM.Attributes;
-
-namespace DreamineApp.Models.PageSub;
-
-public partial class PageSubModel
-{
-	[DreamineProperty]
-	private string _message = "이것은 Sub View Sample 입니다.";
-}
