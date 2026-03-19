dd7bf2b22b2596bb1a4a0a5ea320b4aa6b84796c
msjang <admin@codemaru.co.kr>
2026-03-19 21:34:03 +0900

    Add missing submodules

---
+++ 20_SOURCES/100. Library/Threading.Abstractions/EventArgs/ThreadStatusChangedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading.Abstractions.EventArgs
+{
+    internal class ThreadStatusChangedEventArgs
+    {
+    }
+}
---
+++ 20_SOURCES/100. Library/Threading.Abstractions/Interfaces/IThreadManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threading.Abstractions.Interfaces
+{
+    internal class IThreadManager
+    {
+    }
+}
